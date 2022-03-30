9fef308e21c6c3680711923e3fb160c31838fbfd
Folareen <sakawahab03@gmail.com>
2022-03-30 20:58:48 +0100

    fixed view image button

--- home.js
+++ home.js
@@ -1,24 +1,51 @@
-// let a = ["z","x","c"];
-// let obhh = [a];
 
-// console.log(obhh.indexOf(a));
-// console.log(obhh.a[Math.floor(Math.random() * 3)]);
-// console.log(Math.random());
-// const indexTwo = Math.random() * 4;
 
-// let type = "a";
+// display images
+const BUTTON = document.querySelectorAll('button');
+const BODY = document.querySelector('body');
+// const FILTER = document.querySelector('select');
+// const OPTION = document.querySelector('option')
 
-// let amount = 5;
-// let a = ["z", "x", 'c'];
-// let b =["a", "s", "d"];
-// let c = ["q", "w", "e"];
-// let big = [a,b,c]; type
-// function generateImg(size){
-//     let typeChoosen = size[size.indexOf(eval(type))];
-//     console.log(typeChoosen);
-//     for (let i = 0; i < amount; i++){
-//         console.log(typeChoosen[Math.floor(Math.random()*3)])
-//     }
+// FILTER.addEventListener('change', filterfunc );
+
+// function well(){
+//     alert('it works!!')
 // }
-// generateImg(big);
 
+// function filterfunc(){
+    // console.log(FILTER);
+    // let filterValue = FILTER.V
+    // console.log(OPTION.)
+    // filter.forEach(a => console.log(a.value)); 
+// }
+// filter.forEach(option => {
+//     switch (option){
+//         case "landscape":
+//             console.log(option);
+//     }
+// }); 
+
+
+BUTTON.forEach(each => each.addEventListener('click', displayImage))
+
+
+function displayImage(){
+    let imageContainer = document.createElement('div');
+    imageContainer.classList.add('display');
+    const imageSrc = this.parentElement.firstElementChild.src;
+    let image = document.createElement('img');
+    image.src = imageSrc
+    image.classList.add('display-image');
+    imageContainer.appendChild(image)
+    let closeButton = document.createElement('button');
+    closeButton.textContent = "X";
+    imageContainer.appendChild(closeButton);
+    closeButton.classList.add('close');
+    BODY.appendChild(imageContainer);
+    closeButton.addEventListener('click', closeImage);
+    function closeImage(){
+        this.parentElement.remove();
+        console.log(this.parentElement);
+        console.log(image)
+    }
+}
